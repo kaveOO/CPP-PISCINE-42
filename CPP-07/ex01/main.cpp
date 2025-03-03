4610b9ed45f91b358cd233254b384be77e5f35ea
kaveOO <alban.billiette@gmail.com>
2025-03-03 06:47:12 +0100

    + | Finished ex01

--- CPP-07/ex01/main.cpp
+++ CPP-07/ex01/main.cpp
@@ -2,7 +2,22 @@
 
 void print(char &c)
 {
-	std::cout << c << std::endl;
+	std::cout << c << " ";
+}
+
+void lowerCase(char &c)
+{
+	c += 32;
+}
+
+void print(int &c)
+{
+	std::cout << c << " ";
+}
+
+void plusTen(int &c)
+{
+	c += 10;
 }
 
 int main()
@@ -15,7 +30,29 @@ int main()
 	array[4] = 'O';
 
 
+	std::cout << "************************" << std::endl;
+	std::cout << "| TEST WITH CHAR ARRAY |" << std::endl;
+	std::cout << "************************" << std::endl << std::endl;
+
 	iter(array, 5, print);
+	std::cout << std::endl;
+	iter(array, 5, lowerCase);
+	iter(array, 5, print);
+	std::cout << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "***********************" << std::endl;
+	std::cout << "| TEST WITH INT ARRAY |" << std::endl;
+	std::cout << "***********************" << std::endl;
+
+	int array1[5] = {1, 2, 3, 4, 5};
+
+	std::cout << std::endl;
+	iter(array1, 5, print);
+	iter(array1, 5, plusTen);
+	std::cout << std::endl;
+	iter(array1, 5, print);
+	std::cout << std::endl;
 
 	return (0);
 }
